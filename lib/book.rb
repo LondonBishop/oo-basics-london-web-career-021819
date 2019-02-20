5aad536d535db044ede22a70855148139ca2b7f0
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-20 09:43:29 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,8 +3,8 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title
   
-  def initialize(title)
-    booktitle = titles
+  def initialize(booktitle)
+    title = booktitle
   end
   
   def title
